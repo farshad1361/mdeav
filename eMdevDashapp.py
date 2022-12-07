cc1472b68a4e10f28036ec90a75a22dc47080bc1
farshad1361 <57016090+farshad1361@users.noreply.github.com>
2022-12-07 17:08:52 +0000

    Update eMdevDashapp.py

--- eMdevDashapp.py
+++ eMdevDashapp.py
@@ -117,6 +117,6 @@ def display_filtered_table(year):
 
 # Run local server
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, use_reloader=False)
     #app.run_server(host="0.0.0.0", port=8050, debug=True)
     #app.run_server(debug=True, use_reloader=False)
